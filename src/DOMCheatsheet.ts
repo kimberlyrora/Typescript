7b72190f83406ba0b3fdccb41b2918cd29c8f647
Kimberly Rojas <kimberlyrojasra@gmail.com>
2022-01-10 14:39:55 -0500

    [add]: ts for dom elements w react - on change

--- src/DOMCheatsheet.ts
+++ src/DOMCheatsheet.ts
@@ -2,5 +2,5 @@ import React from "react";
 // For onChangeFn 
 // Resources: https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/
 // Syntax
-const onChangeInput = (e: React.ChangeEventHandler<HTMLInputElement>) => {
-  };
+const onChangeInput = (e: React.ChangeEventHandler<HTMLInputElement>) => {  
+};
