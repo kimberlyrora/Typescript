ab4f754ccdfc7f8cbc664d9be883ed0a307d7ce1
Kimberly Rojas <kimberlyrojasra@gmail.com>
2022-01-10 14:29:55 -0500

    [add]: intro to ts, summary in main.ts

---
+++ src/DOMCheatsheet.ts
@@ -0,0 +1,6 @@
+import React from "react";
+// For onChangeFn 
+// Resources: https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/
+// Syntax
+const onChangeInput = (e: React.ChangeEventHandler<HTMLInputElement>) => {
+  };
