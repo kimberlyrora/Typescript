b6408fd34d8ffb120043b7e71953119fdcee5c8c
Kimberly Rojas <kimberlyrojasra@gmail.com>
2022-01-11 15:14:35 -0500

    [add]: ts type for onchange react and resources tgo solve it

--- src/DOMCheatsheet.ts
+++ src/DOMCheatsheet.ts
@@ -1,6 +1,6 @@
 import React from "react";
 // For onChangeFn 
-// Resources: https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/
+// Resources: https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/, https://stackoverflow.com/questions/42081549/typescript-react-event-types
 // Syntax
-const onChangeInput = (e: React.ChangeEventHandler<HTMLInputElement>) => {  
+const onChangeInput: React.ChangeEventHandler<HTMLInputElement> = (e) => {
 };
